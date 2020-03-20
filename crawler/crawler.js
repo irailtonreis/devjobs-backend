5779037bc0971d3a93b096b64893a1af5a5a9025
almeidarenato <renatoam@ymail.com>
2020-03-20 00:49:17 -0300

    added crawler and yarn search command

---
+++ crawler/crawler.js
@@ -0,0 +1,41 @@
+const puppeteer = require("puppeteer");
+
+const crawler = async tech => {
+  const browser = await puppeteer.launch({
+    headless: true
+  });
+  const page = await browser.newPage();
+
+  await page.goto(
+    `https://www.linkedin.com.br/jobs/search/?f_TPR=r2592000&geoId=106057199&keywords=desenvolvedor%20${tech}&location=Brasil&f_TP=1%2C2%2C3%2C4`
+  );
+  await page.waitForSelector("button.filter-dropdown__button"); //wait for the filter button
+  await page.evaluate(() => {
+    const buttonFilter = document.querySelector(
+      "button.filter-dropdown__button"
+    );
+    buttonFilter.click();
+  });
+  await page.waitForSelector("#TIME_POSTED-2"); //wait for the Last Month filter
+  await page.evaluate(() => {
+    const inputLastMonth = document.querySelector("#TIME_POSTED-2");
+    inputLastMonth.click();
+    const btSubmit = document.querySelector(".filter-button-dropdown__action");
+    btSubmit.click();
+  });
+
+  await page.waitForSelector("span.results-context-header__job-count");
+
+  const jobsCounter = await page.evaluate(() => {
+    return document.querySelector("span.results-context-header__job-count")
+      .innerText;
+  });
+
+  return jobsCounter;
+};
+
+const techs = ["php", "react", "nodejs"];
+
+techs.map(async tech =>
+  console.log(`Jobs encontrados para ${tech}: ` + (await crawler(tech)))
+);
